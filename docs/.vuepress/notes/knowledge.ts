1e14cd5fea6768af4156f6f8bd702c284ba3b8af
Ailurus <ailurus2233@outlook.com>
2025-09-09 11:37:05 +0800

    docs(knowledge): 重构知识库侧边栏结构
    - 修改了知识库侧边栏的层级结构，使其更加清晰
    - 更新了 Prism 样例代码中的区域定义示例
    - 升级了项目中的依赖版本：
        - @vuepress/bundler-vite:2.0.0-rc.23 -> 2.0.0-rc.24
        - sass-embedded: 1.89.2 -> 1.92.1
        - typescript: 5.8.3 -> 5.9.2  - vue: 3.5.16 -> 3.5.21
        - vuepress: 2.0.0-rc.23 -> 2.0.0-rc.24
        - vuepress-theme-plume: 1.0.0-rc.153 -> 1.0.0-rc.162

--- docs/.vuepress/notes/knowledge.ts
+++ docs/.vuepress/notes/knowledge.ts
@@ -1,17 +1,49 @@
-import { ThemeSidebarItem } from "vuepress-theme-plume";
-import { documentSidebar } from "./knowledge/document";
-import { expertiseSidebar } from "./knowledge/expertise";
-import { studySidebar } from "./knowledge/study";
-import { problemSidebar } from "./knowledge/problem";
-import { otherSidebar } from "./knowledge/other";
+import {ThemeSidebarItem} from "vuepress-theme-plume";
+import {documentSidebar} from "./knowledge/document";
+import {expertiseSidebar} from "./knowledge/expertise";
+import {studySidebar} from "./knowledge/study";
+import {problemSidebar} from "./knowledge/problem";
+import {otherSidebar} from "./knowledge/other";
 
 // 定义知识库入门文档的侧边栏
 
 export const knowledgeSideBar = [
-  { text: "知识库主页", link: "/notes/knowledge/" },
-  { text: "入门文档", link: "/notes/knowledge/document/", prefix: "/notes/knowledge/document/", items: documentSidebar },
-  { text: "专业知识", link: "/notes/knowledge/expertise/", prefix: "/notes/knowledge/expertise/", items: expertiseSidebar },
-  { text: "学习笔记", link: "/notes/knowledge/study/", prefix: "/notes/knowledge/study/", items: studySidebar },
-  { text: "问题记录", link: "/notes/knowledge/problem/", prefix: "/notes/knowledge/problem/", items: problemSidebar },
-  { text: "其他归纳", link: "/notes/knowledge/other/", prefix: "/notes/knowledge/other/", items: otherSidebar },
+    {
+        text: "个人知识库", prefix: "/notes/knowledge/", link: "/notes/knowledge/", items: [
+            {
+                text: "入门文档",
+                link: "document/",
+                prefix: "document/",
+                collapsed: true,
+                items: documentSidebar
+            },
+            {
+                text: "专业知识",
+                link: "expertise/",
+                prefix: "expertise/",
+                collapsed: true,
+                items: expertiseSidebar
+            },
+            {
+                text: "学习笔记",
+                link: "study/",
+                prefix: "study/",
+                collapsed: true,
+                items: studySidebar},
+            {
+                text: "问题记录",
+                link: "problem/",
+                prefix: "problem/",
+                collapsed: true,
+                items: problemSidebar
+            },
+            {
+                text: "其他归纳",
+                link: "/other/",
+                prefix: "other/",
+                collapsed: true,
+                items: otherSidebar
+            },]
+    },
+
 ] as ThemeSidebarItem[];
