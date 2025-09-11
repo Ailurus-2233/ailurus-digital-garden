ee8a555c352d560096ec0684466883dd2d41be75
Ailurus2233 <Ailurus2233@outlook.com>
2025-09-11 09:37:01 +0800

    refactor(knowledge): 优化知识库侧边栏节点生成代码格式

--- docs/.vuepress/notes/knowledge.ts
+++ docs/.vuepress/notes/knowledge.ts
@@ -1,19 +1,53 @@
-import {documentItems} from "./knowledge/document";
-import {expertiseItems} from "./knowledge/expertise";
-import {studyItems} from "./knowledge/study";
-import {problemItems} from "./knowledge/problem";
-import {otherItems} from "./knowledge/other";
-import {GenerateNode, GenerateSidebar} from "./sideBarGenerator";
+import { documentItems } from "./knowledge/document";
+import { expertiseItems } from "./knowledge/expertise";
+import { studyItems } from "./knowledge/study";
+import { problemItems } from "./knowledge/problem";
+import { otherItems } from "./knowledge/other";
+import { GenerateNode, GenerateSidebar } from "./sideBarGenerator";
 
+const documentNode = GenerateNode(
+  "入门文档",
+  "document/",
+  "document/",
+  true,
+  documentItems
+);
+const expertiseNode = GenerateNode(
+  "专业知识",
+  "expertise/",
+  "expertise/",
+  true,
+  expertiseItems
+);
+const studyNode = GenerateNode(
+  "学习笔记",
+  "study/",
+  "study/",
+  true,
+  studyItems
+);
+const problemNode = GenerateNode(
+  "问题记录",
+  "problem/",
+  "problem/",
+  true,
+  problemItems
+);
+const otherNode = GenerateNode(
+  "其他归纳",
+  "other/",
+  "other/",
+  true,
+  otherItems
+);
 
-const documentNode = GenerateNode("入门文档", "document/", "document/", true, documentItems);
-const expertiseNode = GenerateNode("专业知识", "expertise/", "expertise/", true, expertiseItems);
-const studyNode = GenerateNode("学习笔记", "study/", "study/", true, studyItems)
-const problemNode = GenerateNode("问题记录", "problem/", "problem/", true, problemItems);
-const otherNode = GenerateNode("其他归纳", "other/", "other/", true, otherItems);
-
-const rootNode = GenerateNode("个人知识库", "", "/notes/knowledge/", undefined,
-    [documentNode, expertiseNode, studyNode, problemNode, otherNode]);
+const rootNode = GenerateNode(
+  "个人知识库",
+  "/notes/knowledge/",
+  "/notes/knowledge/",
+  undefined,
+  [documentNode, expertiseNode, studyNode, problemNode, otherNode]
+);
 
 // 定义知识库入门文档的侧边栏
 export const knowledgeSideBar = GenerateSidebar([rootNode]);
